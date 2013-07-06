41e9d5eb09bcd5905628858c16193c735d97784b
Auke <wiggers.auke@gmail.com>
2013-07-06 17:11:14 +0200

    legend fix wtf

--- graph.py
+++ graph.py
@@ -1,9 +1,10 @@
 import sys
 import glob
 import os
-
-
+import numpy as np
+import matplotlib.pyplot as plt
 import re
+import matplotlib.cm as cm
 
 def tryint(s):
     try:
@@ -22,16 +23,17 @@ def sort_nicely(l):
     """
     l.sort(key=alphanum_key)
 
-if __name__ == "__main__":
-    print "Searching directory {0}.".format( sys.argv[1] )
-
+def parse_one_dir(dirname, maxgen_nr):
     avg = []
     mxs = []
-    filenames = glob.glob(os.path.join(sys.argv[1], '*.txt'))
+    filenames = glob.glob(os.path.join(dirname, '*.txt'))
 
     sort_nicely(filenames)
 
-    for filename in filenames:
+    if len(filenames) < maxgen_nr:
+        print "Insufficient generations in '{0}'".format(dirname)
+
+    for filename in filenames[:maxgen_nr]:
         f = open(filename)
         total = 0
         individuals = 0
@@ -45,20 +47,38 @@ if __name__ == "__main__":
                 individuals += 1
         avg.append(total / float(individuals))
         mxs.append(maximum)
+    return avg, mxs
 
-    import numpy as np
-    import matplotlib.pyplot as plt
+if __name__ == "__main__":
+    if len(sys.argv) != 3 or not sys.argv[2].isdigit():
+        raise Exception('Usage: graph.py top_dirname maxgeneration')
 
-    fig = plt.figure()
-    ax = fig.gca()
-    ax.set_xlabel("Generation Number")
-    ax.set_ylabel("Average fitness")
+    print "Searching directory {0}.".format( sys.argv[1] )
+    dirnames = [o for o in os.listdir(sys.argv[1]) if os.path.isdir(o)]
+    colors = iter( cm.rainbow(np.linspace(0, 1, len(dirnames))))
 
-    xs = np.arange(len(avg))
-    ysavg = np.array(avg)
-    ysmax = np.array(mxs)
+    fig_max = plt.figure()
+    ax_max = fig_max.gca()
+    ax_max.set_xlabel("Generation Number")
+    ax_max.set_ylabel("Maximum fitness")
 
-    ax.plot(xs, ysavg)
+    fig_avg = plt.figure()
+    ax_avg = fig_avg.gca()
+    ax_avg.set_xlabel("Generation Number")
+    ax_avg.set_ylabel("Average fitness")
+
+    for dirname, color in zip(dirnames, colors):
+        avg, mxs = parse_one_dir(dirname, int(sys.argv[2]))
+        xs = np.arange(len(avg))
+        ysavg = np.array(avg)
+        ysmax = np.array(mxs)
+
+        ax_avg.plot(xs, ysavg, color=color)
+        ax_max.plot(xs, ysmax, color=color)
+
+
+    plt.legend(['{}'.format(dirname) for dirname in dirnames], loc=8,
+           ncol=2, mode="expand", bbox_to_anchor=(1.05, 1), borderaxespad=0.)
     plt.show()
 
 
